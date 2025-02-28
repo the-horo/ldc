0b12be7a3afd8c70049f0daf3aa04354d2e18063
Andrei Horodniceanu <a.horodniceanu@proton.me>
2025-02-28 21:29:37 +0200

    debug
    Signed-off-by: Andrei Horodniceanu <a.horodniceanu@proton.me>

--- driver/main.cpp
+++ driver/main.cpp
@@ -275,6 +275,7 @@ void parseCommandLine(Strings &sourceFiles) {
   const auto &exePath = exe_path::getExePath();
   global.params.argv0 = {exePath.length(), exePath.data()};
 
+
   // read config file
   ConfigFile &cfg_file = ConfigFile::instance;
   const char *explicitConfFile = tryGetExplicitConfFile(allArguments);
